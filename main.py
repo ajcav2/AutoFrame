5082e77b42776c64a4051b8fa95bf231bccd9f9f
ajcav2 <ajcav2@gmail.com>
2017-09-26 12:22:14 -0500

    backup

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ from Tkinter import *
 from PIL import Image
 from resizeimage import resizeimage
 from flask import Flask
-from flask_ask import Ask, statement, convert_errors
+from flask_ask import Ask, statement, convert_errors, question
 import logging
 import threading
 import thread
+import io 
+from itertools import cycle
+try:
+    # Python2
+    import Tkinter as tk
+except ImportError:
+    # Python3
+    import tkinter as tk
 
+global slide
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -29,8 +38,13 @@ extras = 'url_c'
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
+@ask.launch
+def begin():
+    return question("What would you like to see photos of")
+
 @ask.intent('getPhotoSubject', mapping={'term': 'photoSubject'})
-def begin(term):
+def respondToUser(term):
+
     # Spawn process on new thread
     download_thread = threading.Thread(target=downloadAndConvert, args=[term])
     download_thread.daemon = True
@@ -44,7 +58,7 @@ def downloadAndConvert(term):
         method = "Flickr"
         
         # Delete all photos in Flickr folder
-        filelist = [ f for f in os.listdir("/home/pi/Documents/AutoFrame/Flickr/") if f.endswith(".gif") ]
+        filelist = [ f for f in os.listdir("/home/pi/Documents/AutoFrame/Flickr/") ]
         for f in filelist:
             print("Removing " + f)
             os.remove("/home/pi/Documents/AutoFrame/Flickr/"+f)
@@ -53,7 +67,7 @@ def downloadAndConvert(term):
         count = 0
 
         # Call Flickr API and parse JSON
-        results = flickr.photos.search(text=term, per_page=50, extras=extras, sort="relevance")
+        results = flickr.photos.search(text=term, per_page=10, extras=extras, sort="relevance")
         for photo in results['photos']['photo']:
             try:
                 url = photo['url_c']
@@ -99,6 +113,7 @@ def downloadAndConvert(term):
                     if test.is_file():
                         continue
                     else:
+                        print("Downloading " + fname + "...")
                         urllib.urlretrieve(pictureObj['images']['standard_resolution']['url'],"/home/pi/Documents/AutoFrame/Instagram/"+fname+".jpg")
 
     # Convert all images to .gif
@@ -119,11 +134,72 @@ def downloadAndConvert(term):
         os.remove("/home/pi/Documents/AutoFrame/"+method+"/"+f)
 
     # Start slideshow
-    os.system('sudo python slideshow.py')
-    return 
+    print("Starting slideshow...")
+    startSlideshow(method)
+    return
+
+def photo_image(self, jpg_filename):
+    with io.open(jpg_filename, 'rb') as ifh:
+        pil_image = Image.open(ifh)
+        return ImageTk.PhotoImage(pil_image)
+
+class App(tk.Tk): # was tk.Tk
+    '''Tk window/label adjusts to size of image'''
+    def __init__(self, image_files, x, y, delay):
+        # the root will be self
+        tk.Tk.__init__(self)
+        # set x, y position only
+        self.geometry("{0}x{1}+0+0".format(self.winfo_screenwidth(), self.winfo_screenheight()))
+        self.state('iconic')
+        
+        self.configure(background='black')
+        self.delay = delay
+        self.bind("<Escape>", lambda e: self.quit())
+        self.focus_force()
+        #self.after(5000, lambda: self.focus_force())
+        #self.wm_attributes("-topmost" , -1)
+        #self.focus_set()
+        # allows repeat cycling through the pictures
+        # store as (img_object, img_name) tuple
+        self.pictures = cycle((tk.PhotoImage(file=image), image)
+                              for image in image_files)
+        self.picture_display = tk.Label(self)
+        self.picture_display.pack()
+    def show_slides(self):
+        '''cycle through the images and show them'''
+        # next works with Python26 or higher
+        img_object, img_name = next(self.pictures)
+        self.picture_display.config(image=img_object)
+        # shows the image filename, but could be expanded
+        # to show an associated description of the image
+        self.title(img_name)
+        self.after(self.delay, self.show_slides)
+    def run(self):
+        self.mainloop()
+    def toggle_geom(self,event):
+        geom=self.master.winfo_geometry()
+        print(geom,self._geom)
+        self.master.geometry(self._geom)
+        self._geom=geom
+
+def startSlideshow(source):
+    # set milliseconds time between slides
+    delay = 3500
+    # get a series of gif images you have in the working folder
+    # or use full path, or set directory to where the images are
+    filelist = [ f for f in os.listdir("/home/pi/Documents/AutoFrame/"+source+"/") ]
+    image_files = []
+    for f in filelist:
+        image_files.append("/home/pi/Documents/AutoFrame/"+source+"/"+f)
+    # upper left corner coordinates of app window
+    x = 100
+    y = 50
+    slide = App(image_files, x, y, delay)
+    slide.show_slides()
+    slide.run()
+    
  
 if __name__ == "__main__":
-#    downloadInsta("anything")
-#    downloadFlickr("ocean")
     port = 5000
     app.run(host='0.0.0.0', port=port)
+    
